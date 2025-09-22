df5c25a86aceb46d99e562b96770fd6349e5a84c
Mehdi Shirani <m3hdi.shirani@gmail.com>
2025-09-22 12:34:54 +0330

    Add testimonials component and related assets
    - Introduced a new Testimonials component to showcase student experiences.
    - Added multiple testimonials with author details and images.
    - Included utility function for managing class names.
    - Added new image assets for testimonials in WEBP format.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { API_URL } from "@/config/global";
 import { CourseSummary } from "@/types/course-summery.interface";
 import CourseCardList from "./(courses)/_components/CourseCardList";
 import Hero from "./_components/Hero";
+import Testimonials from "./_components/Testimonials";
 
 async function getNewestCourse(count: number): Promise<CourseSummary[]> {
   const res = await fetch(`${API_URL}/courses/newest/${count}`, {
@@ -21,6 +22,9 @@ export default async function Home() {
       <Hero />
       <section className="container mx-auto py-8">
         <CourseCardList courses={newestCourses} />
+      </section>
+      <section className="container mx-auto py-8">
+        <Testimonials />
       </section>
     </>
   );
